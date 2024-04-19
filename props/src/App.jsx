bfe651baa5cfa516d8d37d7860db36933eaa3b9f
msabhii <msabhithakur7777@gmail.com>
2024-04-19 22:00:22 +0530

    propLearning

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,31 +1,11 @@
 import "./App.css";
+import Card from "./Card";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-400 bg-slate-600">Hello</h1>
-      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <img
-          class="w-24 h-24 rounded-full mx-auto"
-          src="https://images.pexels.com/photos/3532552/pexels-photo-3532552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          width="384"
-          height="512"
-        />
-        <div class="pt-6 space-y-4">
-          <blockquote>
-            <p class="text-lg font-m">
-              “Tailwind CSS is the only framework that I've seen scale on large
-              teams. It’s easy to customize, adapts to any design, and the build
-              size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption>
-            <div>Sarah Dayan</div>
-            <div>Staff Engineer, Algolia</div>
-          </figcaption>
-        </div>
-      </figure>
+      <h1 className="text-red-400 bg-slate-600 mb-4">Hello</h1>
+      <Card name="abhi" />
     </>
   );
 }
