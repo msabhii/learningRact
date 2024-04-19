f46045e119732742334c4a4656326f886759e615
msabhii <msabhithakur7777@gmail.com>
2024-04-19 20:34:50 +0530

    commiting

--- learningHooks/src/App.jsx
+++ learningHooks/src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState();
-  // let counter = 5;
+  let [counter, setCounter] = useState(1);
+
   return (
     <>
       <h1>mscode</h1>
       <p>Counter: {counter}</p>
       <button
         onClick={() => {
-          counter += 1;
-          console.log("Counter Updated");
+          if (counter < 20) {
+            setCounter(counter + 1);
+            console.log("Counter Updated");
+          }
+          console.log("Greater than 20");
         }}
       >
         OnClick
