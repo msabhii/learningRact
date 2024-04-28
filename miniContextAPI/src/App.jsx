ef1e8892fce87d92e58be806a8fee3870aa471c1
msabhii <msabhithakur7777@gmail.com>
2024-04-28 21:51:05 +0530

    PartOne done

--- miniContextAPI/src/App.jsx
+++ miniContextAPI/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import UserContextProvider from "./Context/UserContextProvider";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>Context</div>
+      <UserContextProvider>
+        <div>
+          <h1>Context👍</h1>
+          <Login />
+          <Profile />
+        </div>
+      </UserContextProvider>
     </>
   );
 }
