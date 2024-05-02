b3c362d596ee741e485d79ef23e7e2fbbb0ea6d1
msabhii <msabhithakur7777@gmail.com>
2024-05-02 21:43:23 +0530

    revision done

--- todocontextLocal/src/App.jsx
+++ todocontextLocal/src/App.jsx
@@ -6,21 +6,16 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos(() => {
-      (prev) => [{ id: Date.now(), ...todo }, ...prev];
-    });
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
-
   const updateTodo = (id, todo) => {
     setTodos((prev) =>
       prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
     );
   };
-
-  const deleteTodo = () => {
-    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prev) => prev.id !== id));
   };
-
   const toggleComplete = (id) => {
     setTodos((prev) =>
       prev.map((prevTodo) =>
