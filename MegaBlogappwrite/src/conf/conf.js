25f3d083fc99cb852439ae3d867bfde0af441bd1
msabhii <msabhithakur7777@gmail.com>
2024-05-14 19:52:33 +0530

    updating

---
+++ MegaBlogappwrite/src/conf/conf.js
@@ -0,0 +1,7 @@
+const conf = {
+  appwriteUrl: String(import.meta.evn.VITE_APPWRITE_URL),
+  appwriteProjectID: String(import.meta.evn.VITE_APPWRITE_PROJECT_ID),
+  appwriteDataBaseID: String(import.meta.evn.VITE_APPWRITE_DATABASE_ID),
+  appwriteBucketID: String(import.meta.evn.VITE_APPWRITE_BUCKET_ID),
+};
+export default conf;
